5f0f6743044319ad455d31903176871ef6f77d81
2020-DEV-009 <>
2020-01-10 19:36:28 +0100

    Added unit tests to check different game results

---
+++ app/src/main/java/com/bnp/tictactoe/extensions/Utils.kt
@@ -0,0 +1,6 @@
+package com.bnp.tictactoe.extensions
+
+import com.bnp.tictactoe.data.Player
+import com.bnp.tictactoe.vm.Result
+
+internal fun Array<String>.checkForWinner(player: Player):Result? = null
