e90143504b1df73bf0d87a202950a96d31b459b0
2020-DEV-009 <>
2020-01-10 15:12:27 +0100

    Create simple UI with gridlayout and custom view for each of the cells

--- app/src/main/java/com/bnp/tictactoe/MainActivity.kt
+++ app/src/main/java/com/bnp/tictactoe/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.bnp.tictactoe
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.bnp.tictactoe.vm.TicTacToeViewModel
+import com.bnp.tictactoe.widget.CellView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val viewModel by lazy { ViewModelProviders.of(this)[TicTacToeViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buildBoard()
+    }
+
+    // Calculate the total number of cells and create/add the views to the board
+    private fun buildBoard() {
+        val boardColumns = resources.getInteger(R.integer.board_columns)
+        val boardRows = resources.getInteger(R.integer.board_rows)
+        val boardCells = (boardColumns * boardRows)-1
+        for (i in 0..boardCells) {
+            boardLayout.addView(CellView(this, i))
+        }
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "Index ${(v as? CellView)?.index}", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/bnp/tictactoe/widget/CellView.kt
@@ -0,0 +1,30 @@
+package com.bnp.tictactoe.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import com.bnp.tictactoe.MainActivity
+import com.bnp.tictactoe.R
+
+/**
+ * Custom view to draw each of the cells inside the Board
+ * Each view has an index to identify the position of the view within the parent
+ */
+internal class CellView @JvmOverloads constructor(
+    context: Context,
+    val index: Int,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+
+    init {
+        //Each cell has fixed size and a border background
+        val size = resources.getDimensionPixelSize(R.dimen.cellSize)
+        layoutParams = LinearLayout.LayoutParams(size, size)
+        background = ContextCompat.getDrawable(context, R.drawable.cell_background)
+        setOnClickListener(context as? MainActivity)
+    }
+
+}
