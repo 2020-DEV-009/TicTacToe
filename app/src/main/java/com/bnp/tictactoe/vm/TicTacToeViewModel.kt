f009974479452427df5b3d55af32f7f2a8e9e269
2020-DEV-009 <>
2020-01-13 12:18:51 +0100

    Added logic to update board data and change turn in ViewModel - All Instrumentation test green

--- app/src/main/java/com/bnp/tictactoe/vm/TicTacToeViewModel.kt
+++ app/src/main/java/com/bnp/tictactoe/vm/TicTacToeViewModel.kt
@@ -3,6 +3,9 @@ package com.bnp.tictactoe.vm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bnp.tictactoe.R
+import com.bnp.tictactoe.data.Player
+import com.bnp.tictactoe.extensions.checkForWinner
 
 internal class TicTacToeViewModel : ViewModel() {
     companion object {
@@ -10,16 +13,39 @@ internal class TicTacToeViewModel : ViewModel() {
         private const val emptyValue = "-"
         private const val xValue = "X"
         private const val oValue = "O"
+
+        internal val player1 = Player("1", xValue, R.drawable.ic_android)
+        internal val player2 = Player("2", oValue, R.drawable.ic_apple)
     }
 
-    private val _gameState: MutableLiveData<GameState> = MutableLiveData()
+    private val _gameState: MutableLiveData<GameState> = MutableLiveData(GameState.Initial)
 
     val gameState: LiveData<GameState>
         get() = _gameState
 
     private val board = Array(9) { emptyValue }
+    internal var currentPlayer = player1
+    private var busyCells = 0
 
     fun onCellClicked(index: Int) {
+        board[index] = currentPlayer.character
+        busyCells++
+        _gameState.postValue(GameState.Playing(board))
+        //There is no possible winner until at least 5 cells will be checked (three checks for the first player)
+        if (busyCells > 4) {
+            board.checkForWinner(currentPlayer)?.apply {
+                _gameState.postValue(GameState.Finished(this))
+                return
+            }
+        }
+        changeTurn()
+    }
 
+    private fun changeTurn() {
+        currentPlayer = if (currentPlayer == player1) {
+            player2
+        } else {
+            player1
+        }
     }
 }
